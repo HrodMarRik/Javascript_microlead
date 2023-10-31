7a93f31e947ed74432d375bdffcb8030ec081f94
Romaric Chanavat <romaric.chanavat@gmail.com>
2023-10-31 11:54:37 +0100

    up

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const dropdowns = Array.from(document.querySelectorAll(".dropdown-btn"));
 
-dropdowns.map((dropdown) => {
-  // Code
+dropdowns.forEach((dropdown) => {
+    const content = dropdown.nextElementSibling; // Sélectionnez le contenu associé à ce bouton
+
+    dropdown.addEventListener("click", function() {
+        if (content.style.display === "none" || content.style.display === "") {
+            content.style.display = "block";
+        } else {
+            content.style.display = "none";
+        }
+    });
 });
