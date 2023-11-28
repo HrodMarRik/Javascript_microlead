bb1594c2f77f6f886570feb9b62e2156f03a5209
Romaric Chanavat <136018863+HrodMarRik@users.noreply.github.com>
2023-11-28 13:58:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-const dropdowns = Array.from(document.querySelectorAll(".dropdown-btn"));
-
-dropdowns.forEach((dropdown) => {
-    const content = dropdown.nextElementSibling; // Sélectionnez le contenu associé à ce bouton
-
-    dropdown.addEventListener("click", function() {
-        if (content.style.display === "none" || content.style.display === "") {
-            content.style.display = "block";
-        } else {
-            content.style.display = "none";
-        }
-    });
-});
+/**
+ * @param {event} evt
+ * @param {string} cityName
+ */
+function openCity(evt, cityName) {
+  console.log(evt, cityName);
+  // code
+}
