347cdc1e7d14294c33cf38c5fb9f0034edc4f4ef
Romaric Chanavat <romaric.chanavat@gmail.com>
2023-11-28 14:27:20 +0100

    auto-up

--- main.js
+++ main.js
@@ -5,4 +5,18 @@
 function openCity(evt, cityName) {
   console.log(evt, cityName);
   // code
+  var tabcontents = document.getElementsByClassName("tabcontent");
+  for (var i = 0; i < tabcontents.length; i++) {
+    tabcontents[i].style.display = "none";
+  }
+
+  var tablinks = document.getElementsByClassName("tablinks");
+  for (var i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(cityName).style.display = "block";
+
+  event.currentTarget.className += " active";
+
 }
